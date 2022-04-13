7de6c0e236aa747238835d2b3b3e60127ca2a324
dionysos <envy@deleuzei>
2022-04-13 11:01:33 +0200

    Yamllinter

--- roles/deployment/terraform/exp1.tf
+++ roles/deployment/terraform/exp1.tf
@@ -1,21 +1,21 @@
 resource "digitalocean_droplet" "exp1" {
-  image = "debian-11-x64"
-  name = "exp1"
-  region = "ams3"
-  size = "s-1vcpu-1gb"
+  image      = "debian-11-x64"
+  name       = "exp1"
+  region     = "ams3"
+  size       = "s-1vcpu-1gb"
   monitoring = "true"
-  tags = ["ansible", "terraform"]
+  tags       = ["ansible", "terraform"]
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
 
 
   connection {
-      host = self.ipv4_address
-      user = "root"
-      type = "ssh"
-      timeout = "2m"
-      agent = "true"
+    host    = self.ipv4_address
+    user    = "root"
+    type    = "ssh"
+    timeout = "2m"
+    agent   = "true"
   }
 
 }
--- roles/deployment/terraform/exp2.tf
+++ roles/deployment/terraform/exp2.tf
@@ -1,18 +1,18 @@
 resource "digitalocean_droplet" "exp2" {
-  image = "debian-11-x64"
-  name = "exp2"
-  region = "ams3"
-  size = "s-1vcpu-1gb"
+  image      = "debian-11-x64"
+  name       = "exp2"
+  region     = "ams3"
+  size       = "s-1vcpu-1gb"
   monitoring = "true"
-  tags = ["ansible", "terraform"]
+  tags       = ["ansible", "terraform"]
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
   connection {
-      host = self.ipv4_address
-      user = "root"
-      type = "ssh"
-      timeout = "2m"
-      agent = "true"
+    host    = self.ipv4_address
+    user    = "root"
+    type    = "ssh"
+    timeout = "2m"
+    agent   = "true"
   }
 }
