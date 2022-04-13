1712e6c9e48e9c7d383034ca5f5fc691a3247b72
dionysos <envy@deleuzei>
2022-04-13 11:58:23 +0200

    Initlized roles

--- roles/deployment/terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "exp1-ip" {
-  description = "IP address of deployed server"
-  value       = digitalocean_droplet.exp1.ipv4_address
-}
-output "exp2-ip" {
-  description = "IP address of deployed server"
-  value       = digitalocean_droplet.exp2.ipv4_address
-}
